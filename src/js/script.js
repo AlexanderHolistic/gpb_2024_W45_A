f12b990ac17229adf30975d627585cd4c4454de7
KaroKK <kuster.karolina@yahoo.com>
2024-11-14 13:44:38 +0100

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -6,18 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const formSubmitButton = document.getElementById("formSubmitButton");
 
   window.editNote = function (noteId, title, content) {
-      formHeader.textContent = "Notiz bearbeiten";
-      formAction.value = "update";
-      formNoteId.value = noteId;
-      document.getElementById("title").value = title;
-      document.getElementById("content").value = content;
-      formSubmitButton.textContent = "Änderungen speichern";
+    formHeader.textContent = "Notiz bearbeiten";
+    formAction.value = "update";
+    formNoteId.value = noteId;
+    document.getElementById("title").value = title;
+    document.getElementById("content").value = content;
+    formSubmitButton.textContent = "Änderungen speichern";
   };
+});
 
-  mainForm.addEventListener("submit", function () {
-      formHeader.textContent = "Neue Notiz hinzufügen";
-      formAction.value = "add";
-      formNoteId.value = "";
-      formSubmitButton.textContent = "Notiz hinzufügen";
-  });
+mainForm.addEventListener("submit", function () {
+  formHeader.textContent = "Neue Notiz hinzufügen";
+  formAction.value = "add";
+  formNoteId.value = "";
+  formSubmitButton.textContent = "Notiz hinzufügen";
 });
